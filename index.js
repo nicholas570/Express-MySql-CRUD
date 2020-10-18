e37aca936126ab84fcef43839b368e850f5b0730
nicholas570 <nicolas.mercier570@gmail.com>
2020-10-18 12:51:37 +0200

    adding put routes

--- index.js
+++ index.js
@@ -23,12 +23,13 @@ app.get('/api/employees', (req, res) => {
   });
 });
 
-app.get('/api/employee/:id', (req, res) => {
-  const { id } = req.params;
+app.get('/api/employees/:id', (req, res) => {
+  const idEmployee = req.params.id;
   connection.query(
-    `SELECT lastname, firstname FROM employee WHERE id = ${id}`,
+    `SELECT * FROM employee WHERE id = ${idEmployee}`,
     (err, result) => {
       if (err) {
+        console.log(err);
         res.status(500).send('Something went wrong');
       } else {
         res.json(result);
@@ -48,14 +49,19 @@ app.get('/api/movies', (req, res) => {
   });
 });
 
-app.get('/api/movies/names', (req, res) => {
-  connection.query('SELECT name FROM movie', (err, result) => {
-    if (err) {
-      res.status(500).send('Something went wrong');
-    } else {
-      res.json(result);
+app.get('/api/movies/:id', (req, res) => {
+  const idMovie = req.params.id;
+  connection.query(
+    `SELECT name FROM movie WHERE id = ${idMovie}`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Something went wrong');
+      } else {
+        res.json(result);
+      }
     }
-  });
+  );
 });
 
 // POST ROUTES
@@ -75,7 +81,7 @@ app.post('/api/employees', (req, res) => {
 });
 
 //MOVIE
-app.post('/api/movie', (req, res) => {
+app.post('/api/movies', (req, res) => {
   const formData = req.body;
 
   connection.query('INSERT INTO movie SET ?', formData, (err, results) => {
@@ -85,6 +91,44 @@ app.post('/api/movie', (req, res) => {
       res.sendStatus(200);
     }
   });
+});
+
+// PUT ROUTES
+
+// EMPLOYEE
+app.put('/api/employees/:id', (req, res) => {
+  const formData = req.body;
+  const { id } = req.params;
+
+  connection.query(
+    'UPDATE employee SET ? WHERE id = ?',
+    [formData, id],
+    (err) => {
+      if (err) {
+        res.status(500).send('Do it again');
+      } else {
+        res.sendStatus(200);
+      }
+    }
+  );
+});
+
+// MOVIE
+app.put('/api/movies/:id', (req, res) => {
+  const formData = req.body;
+  const idEmployee = req.params.id;
+
+  connection.query(
+    'UPDATE movie SET ? WHERE id = ?',
+    [formData, idEmployee],
+    (err) => {
+      if (err) {
+        res.status(500).send('Error');
+      } else {
+        res.sendStatus(200);
+      }
+    }
+  );
 });
 
 app.listen(port, (err) => {
